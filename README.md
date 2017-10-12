93e19fbd5da527cb5e1bc33dda642163fbaca7f1
Eray <eraycetinay@gmail.com>
2017-10-12 23:02:53 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # gulp-parse-assets
+
+it simply grabs files from a defined file and returns back the grabbed vinyl files to the pipe by respecting its position in sequence.
+
+default options;
+	extensions: js,css,html,
+	tags: none.
+
+Eg;
+var gulp=require('gulp');
+var gulpGrab=require('gulp-grab');
+
+//get all js,css and html files
+gulp.src("test/*")
+.pipe(gulpGrab())
+.pipe(gulp.dest("./dist-all"));
+
+//only js files between <!-- start:jsassets --> and <!-- end:jsassets --> tags.
+gulp.src("test/*")
+.pipe(gulpGrab({tags: ['jsassets'],extensions: ['js']}))
+.pipe(gulp.dest("./dist-assets"));
--- index.js
+++ index.js
@@ -1,52 +1,51 @@
 'use strict';
 
 var gulp = require('gulp');
+var util = require('gulp-util');
 var fs = require("fs")
 var path = require('path');
+var through = require('through2');
 
-
-function parseAssets(values) {
+module.exports = function(values) {
     var config = {
-        files: (values.files && values.files[0]) ? values.files : false,
         extensions: ((values.extensions && values.extensions[0]) ? values.extensions : ['js', 'css', 'html']).join('|'),
         tags: (values.tags && values.tags[0]) ? values.tags : false,
     }
-
-    if (!config.files) return [''];
-    var res = [],
-        regex = /([^'"# \(\)\?]+\.(extList))\b/ig;
-    config.files.forEach(function(file) {
-
-        var srcString = fs.readFileSync(file, "utf8");
-        var foundString = '';
-
-
-        if (config.tags) {
-            config.tags.forEach(function(tag) {
-                var tagMatch = /<!-- start:key -->([\s\S]*?)<!-- end:key -->/gmi;
-                var match = new RegExp(tagMatch.source.replace('key', tag), 'gi');
-                match = new RegExp(match.source.replace('key', tag), 'gi');
-                while ((result = match.exec(srcString))) {
-                    foundString += result[1];
-                }
-            });
-            srcString = foundString;
-        }
-
-        var match = new RegExp(regex.source.replace('extList', config.extensions), 'ig');
-
-        var result;
-        while ((result = match.exec(srcString))) {
-            if (!path.isAbsolute(result[1])) {
-                result[1] = path.join(process.cwd(), result[1]);
+    return through.obj(function(file, enc, cb) {
+        if (file.isNull()) {
+            return cb();
+        } else if (file.isStream()) {
+            cb();
+        } else if (file.isBuffer()) {
+            var res = [],
+                regex = /([^'"# \(\)\?]+\.(extList))\b/ig;
+            var srcString = file.contents,foundString,result;
+            if (config.tags) {
+                config.tags.forEach(function(tag) {
+                    var tagMatch = /<!-- start:key -->([\s\S]*?)<!-- end:key -->/gmi;
+                    var match = new RegExp(tagMatch.source.replace('key', tag), 'gi');
+                    match = new RegExp(match.source.replace('key', tag), 'gi');
+                    while ((result = match.exec(srcString))) {
+                        foundString += result[1];
+                    }
+                });
+                srcString = foundString;
             }
-            res.push(result[1]);
+            var match = new RegExp(regex.source.replace('extList', config.extensions), 'ig');
+            while ((result = match.exec(srcString))) {
+                if (!path.isAbsolute(result[1])) {
+                    result[1] = path.join(file.base, result[1]);
+                }
+                if (fs.existsSync(result[1])) {   
+                    var asset = new util.File({
+                        path: result[1],
+                        base: path.parse(result[1]).dir,
+                        contents: fs.readFileSync(result[1])
+                    });
+                    this.push(asset);
+                }
+            }
+            cb();
         }
-
     });
-
-    return res;
 }
-console.log(parseAssets({ files: ['test/test.html'], tags: ['assets'] }));
-
-module.exports = parseAssets;
