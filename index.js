777686b6b7830429c304f3cdb226185f3486f295
Eray Çetinay <eraycetinay@gmail.com>
2017-10-12 10:36:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
+'use strict';
+
 var gulp = require('gulp');
 var fs = require("fs")
 var path = require('path');
 
 
 function parseAssets(values) {
-
-	//split('<!-- build:bundleJs -->')[1]).split('<!-- endbuild -->')[0]
-
     var config = {
         files: (values.files && values.files[0]) ? values.files : false,
         extensions: ((values.extensions && values.extensions[0]) ? values.extensions : ['js', 'css', 'html']).join('|'),
-        tags: (values.tags && values.tags[0]) ? values.tags : '',
+        tags: (values.tags && values.tags[0]) ? values.tags : false,
     }
 
-    if (!config.files) return ['']; 
-    var res = [], regex = /([^'"# \(\)\?]+\.(extList))\b/ig;
+    if (!config.files) return [''];
+    var res = [],
+        regex = /([^'"# \(\)\?]+\.(extList))\b/ig;
     config.files.forEach(function(file) {
 
-        var srcString = fs.readFileSync(file, "utf8"); 
+        var srcString = fs.readFileSync(file, "utf8");
+        var foundString = '';
+
+
+        if (config.tags) {
+            config.tags.forEach(function(tag) {
+                var tagMatch = /<!-- start:key -->([\s\S]*?)<!-- end:key -->/gmi;
+                var match = new RegExp(tagMatch.source.replace('key', tag), 'gi');
+                match = new RegExp(match.source.replace('key', tag), 'gi');
+                while ((result = match.exec(srcString))) {
+                    foundString += result[1];
+                }
+            });
+            srcString = foundString;
+        }
+
         var match = new RegExp(regex.source.replace('extList', config.extensions), 'ig');
 
         var result;
@@ -28,11 +43,10 @@ function parseAssets(values) {
             res.push(result[1]);
         }
 
-
     });
 
     return res;
 }
+console.log(parseAssets({ files: ['test/test.html'], tags: ['assets'] }));
 
-
-console.log(parseAssets({files:['test.html','b.js'], extensions:['js']}));
+module.exports = parseAssets;
